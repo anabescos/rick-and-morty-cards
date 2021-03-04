b7705c435763201664fca8d4432377e7eb952a88
anabescos <anabes88@gmail.com>
2021-03-04 20:22:05 +0100

    Add non existent character message

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,36 +1,44 @@
 import { Link } from "react-router-dom";
 
 function CharacterDetail(props) {
-  return (
-    <section className="details">
-      <Link className="details__link" to="/">
-        {" "}
-        Return{" "}
-      </Link>
-      <img
-        className="details__img"
-        src={props.selectedCharacter.image}
-        alt={props.selectedCharacter.name}
-      />
-      <ul className="details__list">
-        <li className="details__list--name">
-          Name:{props.selectedCharacter.name}
-        </li>
-        <li className="details__list--gender">
-          Gender:{props.selectedCharacter.gender}{" "}
-        </li>
-        <li className="details__list--species">
-          Species:{props.selectedCharacter.species}
-        </li>
-        <li className="details__list--origin">
-          Origin:{props.selectedCharacter.origin}
-        </li>
-        <li className="details__list--episodes">
-          Number of episodes featured:{props.selectedCharacter.episodes}
-        </li>
-      </ul>
-    </section>
-  );
+  if (props.selectedCharacter === undefined) {
+    return (
+      <div>
+        <img src="../images/PickleRick.png" alt="character not found" />
+      </div>
+    );
+  } else {
+    return (
+      <section className="details">
+        <Link className="details__link" to="/">
+          {" "}
+          Return{" "}
+        </Link>
+        <img
+          className="details__img"
+          src={props.selectedCharacter.image}
+          alt={props.selectedCharacter.name}
+        />
+        <ul className="details__list">
+          <li className="details__list--name">
+            Name:{props.selectedCharacter.name}
+          </li>
+          <li className="details__list--gender">
+            Gender:{props.selectedCharacter.gender}{" "}
+          </li>
+          <li className="details__list--species">
+            Species:{props.selectedCharacter.species}
+          </li>
+          <li className="details__list--origin">
+            Origin:{props.selectedCharacter.origin}
+          </li>
+          <li className="details__list--episodes">
+            Number of episodes featured:{props.selectedCharacter.episodes}
+          </li>
+        </ul>
+      </section>
+    );
+  }
 }
 
 export default CharacterDetail;
